ca5960641b0d3a688ddc98d28f984386505e69a5
franpog859 <franciszek.pog@gmail.com>
2020-02-19 18:01:32 +0100

    Get position and update every second

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import SunCalc from 'suncalc'
 
 function App() {
+  const [date, setDate] = useState(new Date())
+  const [position, setPosition] = useState()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date())
+      setAsyncPosition()
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  const setAsyncPosition = (() => { navigator.geolocation.getCurrentPosition(
+    position => setPosition(position),
+    err => console.log("Failed to get current position", err)
+  )})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{SunCalc.getTimes(date, position?.coords.latitude, position?.coords.longitude).goldenHour.toString()}</p>
+        <p>{date?.toString()}</p>
+        <p>{position?.toString()}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
