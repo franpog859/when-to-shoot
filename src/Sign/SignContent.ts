7029abcf7bd63f306e5d98b0fc01eb83f9d676a1
franpog859 <franciszek.pog@gmail.com>
2020-02-26 13:47:58 +0100

    Refactor Content to have percents also in NoGH state

--- src/Sign/SignContent.ts
+++ src/Sign/SignContent.ts
@@ -13,56 +13,68 @@ export function GetSignContent(date: Date, position?: Position): SignContent {
 
     const goldenHours = GetGoldenHours(date, position)
 
-    if (date.getTime() >= goldenHours.morningGoldenHourStarts.getTime() && 
-        date.getTime() < goldenHours.morningGoldenHourEnds.getTime()) {
+    if (date.getTime() >= goldenHours.morningGoldenHourStart.getTime() && 
+        date.getTime() < goldenHours.morningGoldenHourEnd.getTime()) {
 
         return YesGoldenHourResponse(
             date, 
-            goldenHours.morningGoldenHourStarts,
-            goldenHours.morningGoldenHourEnds,
+            goldenHours.morningGoldenHourStart,
+            goldenHours.morningGoldenHourEnd,
         )
     }
 
-    if (date.getTime() >= goldenHours.eveningGoldenHourStarts.getTime() && 
-        date.getTime() < goldenHours.eveningGoldenHourEnds.getTime()) {
+    if (date.getTime() >= goldenHours.eveningGoldenHourStart.getTime() && 
+        date.getTime() < goldenHours.eveningGoldenHourEnd.getTime()) {
 
         return YesGoldenHourResponse(
             date, 
-            goldenHours.eveningGoldenHourStarts,
-            goldenHours.eveningGoldenHourEnds,
+            goldenHours.eveningGoldenHourStart,
+            goldenHours.eveningGoldenHourEnd,
         )
     }
 
-    if (date.getTime() < goldenHours.morningGoldenHourStarts.getTime()) {
-        return NoGoldenHourResponse(date, goldenHours.morningGoldenHourStarts)
+    if (date.getTime() < goldenHours.morningGoldenHourStart.getTime()) {
+        return NoGoldenHourPreviousDayResponse(
+            date, 
+            goldenHours.morningGoldenHourStart,
+            goldenHours.eveningGoldenHourEnd,
+        )
     }
 
-    if (date.getTime() < goldenHours.eveningGoldenHourStarts.getTime()) {
-        return NoGoldenHourResponse(date, goldenHours.eveningGoldenHourStarts)
+    if (date.getTime() < goldenHours.eveningGoldenHourStart.getTime()) {
+        return NoGoldenHourResponse(
+            date, 
+            goldenHours.eveningGoldenHourStart,
+            goldenHours.morningGoldenHourEnd,
+        )
     }
 
-    if (date.getTime() >= goldenHours.eveningGoldenHourEnds.getTime()) {
-        return NoGoldenHourNextDayResponse(date, goldenHours.morningGoldenHourStarts)
+    if (date.getTime() >= goldenHours.eveningGoldenHourEnd.getTime()) {
+        return NoGoldenHourNextDayResponse(
+            date, 
+            goldenHours.morningGoldenHourStart,
+            goldenHours.eveningGoldenHourEnd,
+        )
     }
 
     return NoGoldenHourSomethingWrongResponse()
 }
 
 interface GoldenHourTimes {
-    morningGoldenHourStarts: Date;
-    morningGoldenHourEnds: Date;
-    eveningGoldenHourStarts: Date;
-    eveningGoldenHourEnds: Date;
+    morningGoldenHourStart: Date;
+    morningGoldenHourEnd: Date;
+    eveningGoldenHourStart: Date;
+    eveningGoldenHourEnd: Date;
 }
 
 function GetGoldenHours(date: Date, position: Position): GoldenHourTimes {
     const times = SunCalc.getTimes(date, position.coords.latitude, position.coords.longitude)
 
     return {
-        morningGoldenHourStarts: times.sunriseEnd,
-        morningGoldenHourEnds: times.goldenHourEnd,
-        eveningGoldenHourStarts: times.goldenHour,
-        eveningGoldenHourEnds: times.sunsetStart,
+        morningGoldenHourStart: times.sunriseEnd,
+        morningGoldenHourEnd: times.goldenHourEnd,
+        eveningGoldenHourStart: times.goldenHour,
+        eveningGoldenHourEnd: times.sunsetStart,
     }
 }
 
@@ -77,33 +89,46 @@ function NoGoldenHourNoPositionResponse(): SignContent {
 function YesGoldenHourResponse(date: Date, goldenHourStart: Date, goldenHourEnd: Date): SignContent {
     return {
         suggestion: 'Right now!',
-        information: 'Golden hour ends at ' + goldenHourEnd.toLocaleTimeString(),
-        timer: new Date(goldenHourEnd.getTime() - date.getTime()).toLocaleTimeString(),
+        information: 'Golden hour ends at ' + goldenHourEnd.toLocaleTimeString('en-US'),
+        timer: new Date(goldenHourEnd.getTime() - date.getTime()).toLocaleTimeString('en-GB'),
         isGoldenHour: true,
-        goldenHourPercent: GetGoldenHourPercent(date, goldenHourStart, goldenHourEnd)
+        goldenHourPercent: GetDatePercent(date, goldenHourStart, goldenHourEnd)
     }
 }
 
-function GetGoldenHourPercent(date: Date, goldenHourStart: Date, goldenHourEnd: Date): number {
+function GetDatePercent(date: Date, goldenHourStart: Date, goldenHourEnd: Date): number {
     return 100 * (date.getTime() - goldenHourStart.getTime()) / (goldenHourEnd.getTime() - goldenHourStart.getTime())
 }
 
-function NoGoldenHourResponse(date: Date, goldenHourStart: Date): SignContent {
+function NoGoldenHourPreviousDayResponse(date: Date, goldenHourStart: Date, goldenHourEnd: Date): SignContent {
+    const previousEveningGoldenHourEnd = new Date(goldenHourEnd.getTime() - (1000 * 60 * 60 * 24))
     return {
         suggestion: 'Wait for it...',
-        information: 'Golden hour starts at ' + goldenHourStart.toLocaleTimeString(),
-        timer: new Date(goldenHourStart.getTime() - date.getTime()).toLocaleTimeString(),
-        isGoldenHour: false
+        information: 'Golden hour starts at ' + goldenHourStart.toLocaleTimeString('en-US'),
+        timer: new Date(goldenHourStart.getTime() - date.getTime()).toLocaleTimeString('en-GB'),
+        isGoldenHour: false,
+        goldenHourPercent: GetDatePercent(date, previousEveningGoldenHourEnd, goldenHourStart)
     }
 }
 
-function NoGoldenHourNextDayResponse(date: Date, goldenHourStart: Date): SignContent {
+function NoGoldenHourResponse(date: Date, goldenHourStart: Date, goldenHourEnd: Date): SignContent {
+    return {
+        suggestion: 'Wait for it...',
+        information: 'Golden hour starts at ' + goldenHourStart.toLocaleTimeString('en-US'),
+        timer: new Date(goldenHourStart.getTime() - date.getTime()).toLocaleTimeString('en-GB'),
+        isGoldenHour: false,
+        goldenHourPercent: GetDatePercent(date, goldenHourEnd, goldenHourStart)
+    }
+}
+
+function NoGoldenHourNextDayResponse(date: Date, goldenHourStart: Date, goldenHourEnd: Date): SignContent {
     const nextMorningGoldenHourStart = new Date(goldenHourStart.getTime() + (1000 * 60 * 60 * 24))
     return {
         suggestion: 'Wait for it...',
-        information: 'Golden hour starts at ' + nextMorningGoldenHourStart.toLocaleTimeString(),
-        timer: new Date(nextMorningGoldenHourStart.getTime() - date.getTime()).toLocaleTimeString(),
-        isGoldenHour: false
+        information: 'Golden hour starts at ' + nextMorningGoldenHourStart.toLocaleTimeString('en-US'),
+        timer: new Date(nextMorningGoldenHourStart.getTime() - date.getTime()).toLocaleTimeString('en-GB'),
+        isGoldenHour: false,
+        goldenHourPercent: GetDatePercent(date, goldenHourEnd, goldenHourStart)
     }
 }
 
